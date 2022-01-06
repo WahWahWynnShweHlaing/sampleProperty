bb392c427d0756bbafe1af2a1f7bf3898400f385
P-000242\WahWahWinShweHlaing <scm.wahwahwynnshwehlaing@gmail.com>
2022-01-06 21:52:20 +0630

    code format commit

--- js/style.js
+++ js/style.js
@@ -1,15 +1,15 @@
 $(function () {
-  $(document).on('click', '.menuBar', function () {
-    $('.sp-gnav').slideToggle('slow');
-    $(this).toggleClass('is-active');
+  $(document).on("click", ".menuBar", function () {
+    $(".sp-gnav").slideToggle("slow");
+    $(this).toggleClass("is-active");
     return false;
   });
 });
 
 $(function () {
   $(".dropdown-list").mouseover(function () {
-    $('.dropdown-lists').slideToggle('slow');
-    $(this).toggleClass('is-active');
+    $(".dropdown-lists").slideToggle("slow");
+    $(this).toggleClass("is-active");
     return false;
   });
 });
